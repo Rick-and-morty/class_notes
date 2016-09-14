ee61a163824f06a31ea9b3b134bf130c0e578545
Rick-and-morty <te5840@gmail.com>
2016-09-14 12:00:34 -0400

    added lists and dictionaries

--- day3.py
+++ day3.py
@@ -1,3 +1,13 @@
 
 for _ in range(5):
     print("i am looping")
+
+turns = 0
+while turns < 5:
+    print("while looping")
+    turns = turns + 1
+
+choice = ""
+while choice != "n":
+    print(choice)
+    choice = input("do you want to keep looking? [Y/n] ")
---
+++ day3_collections.py
@@ -0,0 +1,27 @@
+
+my_list = ["tyler", 23, ["the future", "burgulars"]]
+
+print(my_list)
+my_list.pop
+my_list.append("max")
+print(my_list)
+print(my_list[1])
+print(my_list[2])
+print(my_list[2][0])
+
+my_dicitionary = {
+    "name": "tyler",
+    "age": 33,
+    "fears": ["the future", "burgulars"]
+}
+
+print(my_dicitionary["name"])
+print(my_dicitionary["age"])
+print(my_dicitionary["fears"])
+
+print(dir(my_list))
+print(help(my_list))
+print(help(my_dicitionary))
+
+
+print(my_dicitionary.keys())
